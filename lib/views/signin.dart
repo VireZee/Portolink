b4ca37461ec563f6e5b27fab03a5d87a8a642562
unknown <jemmanuel@student.ciputra.ac.id>
2022-01-19 22:18:40 +0700

    Sign In Form Key

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -7,9 +7,11 @@ class Signin extends StatefulWidget {
   _SigninState createState() => _SigninState();
 }
 class _SigninState extends State<Signin> {
+  final _formKey = GlobalKey<FormState>();
   final ctrlEmail = TextEditingController();
   final ctrlPass = TextEditingController();
-  bool on = true;
+  bool vis = true;
+  bool load = false;
   @override
   void dispose() {
     ctrlEmail.dispose();
@@ -20,188 +22,191 @@ class _SigninState extends State<Signin> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/signin_bg.jpg'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken)
-            )
-          )
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
-            children: [
-              Flexible(
-                child: Center(
-                  child: Column(
+        Form(
+          key: _formKey,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/signin_bg.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken)
+              )
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Column(
+                children: [
+                  Flexible(
+                    child: Center(
+                      child: Column(
+                        children: const [
+                          Spacer(flex: 10),
+                          Text(
+                            'Portolink',
+                            style: TextStyle(
+                              fontFamily: 'Dancing Script',
+                              color: Color(0xFF0000FF),
+                              fontSize: 80
+                            )
+                          ),
+                          Spacer()
+                        ]
+                      )
+                    )
+                  ),
+                  const Flexible(
+                    child: Center(
+                      child: Text(
+                        'SIGN IN',
+                        style: TextStyle(
+                          fontFamily: 'Oswald',
+                          fontSize: 60
+                        )
+                      )
+                    )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[500]?.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Center(
+                        child: TextField(
+                          style: const TextStyle(fontSize: 25),
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 25),
+                              child: Icon(
+                                FontAwesomeIcons.envelope,
+                                size: 28
+                              )
+                            ),
+                            hintText: 'Email'
+                          ),
+                          controller: ctrlEmail,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next
+                        )
+                      )
+                    )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[500]?.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Center(
+                        child: TextField(
+                          style: const TextStyle(fontSize: 25),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 25),
+                              child: Icon(
+                                FontAwesomeIcons.lock,
+                                size: 28
+                              )
+                            ),
+                            hintText: 'Password',
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  vis = !vis;
+                                });
+                              },
+                              child: Icon(
+                                vis
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                              )
+                            )
+                          ),
+                          obscureText: vis,
+                          controller: ctrlPass,
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.done,
+                        )
+                      )
+                    )
+                  ),
+                  Row(
                     children: const [
                       Spacer(flex: 10),
                       Text(
-                        'Portolink',
+                        'Forgot Password',
                         style: TextStyle(
-                          fontFamily: 'Dancing Script',
-                          color: Color(0xFF0000FF),
-                          fontSize: 80
+                          fontFamily: 'Roboto',
+                          fontSize: 15
                         )
                       ),
                       Spacer()
                     ]
-                  )
-                )
-              ),
-              const Flexible(
-                child: Center(
-                  child: Text(
-                    'SIGN IN',
-                    style: TextStyle(
-                      fontFamily: 'Oswald',
-                      fontSize: 60
-                    )
-                  )
-                )
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                child: Container(
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[500]?.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(50)
                   ),
-                  child: Center(
-                    child: TextField(
-                      style: const TextStyle(fontSize: 25),
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25),
-                          child: Icon(
-                            FontAwesomeIcons.envelope,
-                            size: 28
-                          )
-                        ),
-                        hintText: 'Email'
-                      ),
-                      controller: ctrlEmail,
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next
-                    )
-                  )
-                )
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                child: Container(
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[500]?.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(50)
-                  ),
-                  child: Center(
-                    child: TextField(
-                      style: const TextStyle(fontSize: 25),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25),
-                          child: Icon(
-                            FontAwesomeIcons.lock,
-                            size: 28
-                          )
-                        ),
-                        hintText: 'Password',
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              on = !on;
-                            });
-                          },
-                          child: Icon(
-                            on
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                          )
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 40,
+                    width: 150,
+                    child: ElevatedButton.icon(
+                      onPressed: ctrlEmail.text.isNotEmpty && ctrlPass.text.isNotEmpty
+                      ? () {}
+                      : null,
+                      style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.resolveWith((states) {
+                          return states.contains(MaterialState.pressed)
+                          ? Colors.blue
+                          : null;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith((states) {
+                          return states.contains(MaterialState.pressed)
+                          ? const Color(0xFF00FF00)
+                          : null;
+                        }),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
                         )
                       ),
-                      obscureText: on,
-                      controller: ctrlPass,
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.done,
-                    )
-                  )
-                )
-              ),
-              Row(
-                children: const [
-                  Spacer(flex: 10),
-                  Text(
-                    'Forgot Password',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 15
+                      icon: const Icon(Icons.login),
+                      label: Row(
+                        children: const [
+                          Spacer(),
+                          Text(
+                            'SIGN IN',
+                            style: TextStyle(
+                              fontFamily: 'Prompt',
+                              fontSize: 20
+                            )
+                          ),
+                          Spacer(flex: 2)
+                        ]
+                      )
                     )
                   ),
-                  Spacer()
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, Signup.routeName);
+                    },
+                    child: const Text(
+                      'Create New Account',
+                      style: TextStyle(
+                        fontFamily: 'Flamenco',
+                        fontSize: 30,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1
+                      )
+                    )
+                  ),
+                  const SizedBox(height: 20)
                 ]
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 40,
-                width: 150,
-                child: ElevatedButton.icon(
-                  onPressed: ctrlEmail.text.isNotEmpty && ctrlPass.text.isNotEmpty
-                  ? () {}
-                  : null,
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.resolveWith((states) {
-                      return states.contains(MaterialState.pressed)
-                      ? Colors.blue
-                      : null;
-                    }),
-                    foregroundColor: MaterialStateProperty.resolveWith((states) {
-                      return states.contains(MaterialState.pressed)
-                      ? const Color(0xFF00FF00)
-                      : null;
-                    }),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                    )
-                  ),
-                  icon: const Icon(Icons.login),
-                  label: Row(
-                    children: const [
-                      Spacer(),
-                      Text(
-                        'SIGN IN',
-                        style: TextStyle(
-                          fontFamily: 'Prompt',
-                          fontSize: 20
-                        )
-                      ),
-                      Spacer(flex: 2)
-                    ]
-                  )
-                )
-              ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, Signup.routeName);
-                },
-                child: const Text(
-                  'Create New Account',
-                  style: TextStyle(
-                    fontFamily: 'Flamenco',
-                    fontSize: 30,
-                    decoration: TextDecoration.underline,
-                    decorationThickness: 1
-                  )
-                )
-              ),
-              const SizedBox(height: 20)
-            ]
+              )
+            )
           )
         )
       ]
