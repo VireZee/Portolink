9c0838ddc3de500737fba9187ae2483b5064f9f6
unknown <jemmanuel@student.ciputra.ac.id>
2022-01-28 23:50:12 +0700

    Edit

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -9,9 +9,9 @@ class _ProfileState extends State<Profile> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Auth.getUser(),
-      builder: (context, snapshot) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Auth.uCollection.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) {
           return Container(
             decoration: const BoxDecoration(
@@ -41,29 +41,60 @@ class _ProfileState extends State<Profile> {
             child: Activity.loading()
           );
         }
-        // StreamBuilder<QuerySnapshot>(
-        //   stream: Auth.uCollection.snapshots(),
-        //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        //     return Stack(
-        //       children: snapshot.data!.docs.map((DocumentSnapshot doc) {
-        //         final Users users = Users(
-        //           doc['uid'],
-        //           doc['photo'],
-        //           doc['name'],
-        //           doc['phone'],
-        //           doc['email'],
-        //           doc['password'],
-        //           doc['created'],
-        //           doc['updated'],
-        //           doc['entered'],
-        //           doc['left']
-        //         );
-        //         return ProfileView(users: users);
-        //       }).toList()
-        //     );
-        //   }
-        // );
-        return ProfileView(users: snapshot.data! as Users);
+        if (snapshot.hasData) {
+          return FutureBuilder(
+            future: Auth.getUser(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/profile_bg.jpg'),
+                      fit: BoxFit.fill
+                    )
+                  ),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/no_net_bg.png'),
+                        fit: BoxFit.fitWidth
+                      )
+                    )
+                  )
+                );
+              }
+              else if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/profile_bg.jpg'),
+                      fit: BoxFit.fill  
+                    )
+                  ),
+                  child: Activity.loading()
+                );
+              }
+              return ProfileView(users: snapshot.data! as Users);
+            }
+          );
+        }
+        return Stack(
+          children: snapshot.data!.docs.map((DocumentSnapshot doc) {
+            final Users users = Users(
+              doc['uid'],
+              doc['photo'],
+              doc['name'],
+              doc['phone'],
+              doc['email'],
+              doc['password'],
+              doc['created'],
+              doc['updated'],
+              doc['entered'],
+              doc['left']
+            );
+            return ProfileView(users: users);
+          }).toList()
+        );
       }
     );
   }
