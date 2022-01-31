0b0d05180e55685ac50839561ca4215c7ba33190
unknown <jemmanuel@student.ciputra.ac.id>
2022-01-31 21:41:04 +0700

    Services

--- lib/services/orders.dart
+++ lib/services/orders.dart
@@ -1,1 +1,7 @@
 part of 'services.dart';
+
+class Orders {
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  static CollectionReference oCollection = FirebaseFirestore.instance.collection('Orders');
+  static CollectionReference pCollection = FirebaseFirestore.instance.collection('Pendings');
+}
