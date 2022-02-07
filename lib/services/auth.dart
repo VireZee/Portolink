fc4cca95b3f73317666ed0cf2dbd243fd1a1694d
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-07 18:33:46 +0700

    Auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -30,7 +30,7 @@ class Auth {
         'Email': users.email.replaceAll(' ', '').toLowerCase(),
         'Password': sha512.convert(utf8.encode(sha512.convert(utf8.encode(users.password)).toString())).toString(),
         'Token': token,
-        'Superuser': false,
+        'Administrator': false,
         'Created': dateNow,
         'Updated': '-',
         'Entered': '-',
