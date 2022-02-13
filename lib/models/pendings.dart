3ba48a338a31ba138aa6244bde8244e2fb43890f
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-14 03:38:57 +0700

    Delete unused codes, Formatting

--- lib/models/pendings.dart
+++ lib/models/pendings.dart
@@ -1,14 +1,6 @@
 part of 'models.dart';
 
 class Pendings {
-  final String status;
   final String text;
-  const Pendings(
-    this.status,
-    this.text
-  );
-  List<Object> get props => [
-    status,
-    text
-  ];
+  const Pendings(this.text);
 }
--- lib/models/templates.dart
+++ lib/models/templates.dart
@@ -5,16 +5,5 @@ class Templates {
   final String name;
   final String desc;
   final String price;
-  const Templates(
-    this.photo,
-    this.name,
-    this.desc,
-    this.price
-  );
-  List<Object> get props => [
-    photo,
-    name,
-    desc,
-    price
-  ];
+  const Templates(this.photo, this.name, this.desc, this.price);
 }
