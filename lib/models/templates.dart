b996d8f545cc8bfecb09f1cb462e78c063b30757
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-18 16:55:39 +0700

    Numbers

--- lib/models/templates.dart
+++ lib/models/templates.dart
@@ -4,6 +4,6 @@ class Templates {
   final String photo;
   final String name;
   final String desc;
-  final String price;
+  final int price;
   const Templates(this.photo, this.name, this.desc, this.price);
 }
