0fd0f1cff14a5bdddb35ccd9bb052ab76f295182
unknown <jemmanuel@student.ciputra.ac.id>
2022-05-01 19:32:55 +0700

    Request

--- lib/views/request.dart
+++ lib/views/request.dart
@@ -56,6 +56,264 @@ class _RequestState extends State<Request> {
   }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: <Container>[
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/order_bg.jpg'),
+              fit: BoxFit.fill
+            )
+          ),
+          child: Scaffold(
+            appBar: AppBar(
+              leading: const BackButton(),
+              backgroundColor: Colors.transparent,
+              toolbarHeight: 75,
+              elevation: 0,
+              title: const Text('Request Custom Template'),
+              centerTitle: true
+            ),
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: <ListView>[
+                ListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  physics: const BouncingScrollPhysics(),
+                  children: <Form>[
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            'Description',
+                            style: TextStyle(fontSize: 15)
+                          ),
+                          const SizedBox(height: 8),
+                          TextField(
+                            onChanged: (value) => isEmpty(),
+                            controller: ctrlDesc,
+                            keyboardType: TextInputType.text,
+                            style: const TextStyle(fontSize: 20),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
+                            ),
+                            maxLines: 3,
+                            textInputAction: TextInputAction.next
+                          ),
+                          const SizedBox(height: 16),
+                          const Text(
+                            'Contact',
+                            style: TextStyle(fontSize: 15)
+                          ),
+                          const SizedBox(height: 8),
+                          TextField(
+                            onChanged: (value) => isEmpty(),
+                            controller: ctrlContact,
+                            keyboardType: TextInputType.text,
+                            style: const TextStyle(fontSize: 20),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
+                            ),
+                            maxLines: 2,
+                            textInputAction: TextInputAction.done
+                          ),
+                          const SizedBox(height: 16),
+                          imgFile == null
+                          ? Row(
+                            children: <Widget>[
+                              ElevatedButton.icon(
+                                onPressed: () => chooseFile(),
+                                icon: const Icon(Icons.image),
+                                label: const Text('Pick Image'),
+                                style: ButtonStyle(
+                                  overlayColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? Colors.blue
+                                    : null;
+                                  }),
+                                  foregroundColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? const Color(0xFF00FF00)
+                                    : null;
+                                  }),
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))
+                                  )
+                                )
+                              ),
+                              const SizedBox(width: 16),
+                              const Text('File not found.', style: TextStyle(color: Colors.red))
+                            ]
+                          )
+                          : Row(
+                            children: <Widget>[
+                              ElevatedButton.icon(
+                                onPressed: () => chooseFile(),
+                                icon: const Icon(Icons.image),
+                                label: const Text('Repick'),
+                                style: ButtonStyle(
+                                  overlayColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? Colors.blue
+                                    : null;
+                                  }),
+                                  foregroundColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? const Color(0xFF00FF00)
+                                    : null;
+                                  }),
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))
+                                  )
+                                )
+                              ),
+                              const SizedBox(width: 16),
+                              Semantics(child: Image.file(File(imgFile!.path), width: 100))
+                            ]
+                          ),
+                          const SizedBox(height: 80),
+                          Center(
+                            child: SizedBox(
+                              height: 50,
+                              width: 250,
+                              child: ElevatedButton.icon(
+                                onPressed: isEmpty()
+                                ? () async {
+                                  setState(() => load = true);
+                                  FocusScope.of(context).requestFocus(FocusNode());
+                                  final ConnectivityResult net = await (Connectivity().checkConnectivity());
+                                  final bool sub = await InternetConnectionChecker().hasConnection;
+                                  if (net == ConnectivityResult.none) {
+                                    setState(() => load = false);
+                                    ft.showToast(
+                                      child: Activity.showToast(
+                                        'No internet connection',
+                                        const Color(0xFFFF0000)
+                                      ),
+                                      toastDuration: const Duration(seconds: 1),
+                                      fadeDuration: 200
+                                    );
+                                  }
+                                  else if (ctrlDesc.text.isEmpty) {
+                                    setState(() => load = false);
+                                    ft.showToast(
+                                      child: Activity.showToast(
+                                        'Color can\'t be empty',
+                                        const Color(0xFFFF0000)
+                                      ),
+                                      toastDuration: const Duration(seconds: 1),
+                                      fadeDuration: 200
+                                    );
+                                  }
+                                  else if (ctrlContact.text.isEmpty) {
+                                    setState(() => load = false);
+                                    ft.showToast(
+                                      child: Activity.showToast(
+                                        'Contact can\'t be empty',
+                                        const Color(0xFFFF0000)
+                                      ),
+                                      toastDuration: const Duration(seconds: 1),
+                                      fadeDuration: 200
+                                    );
+                                  }
+                                  else if (imgFile == null) {
+                                    setState(() => load = false);
+                                    ft.showToast(
+                                      child: Activity.showToast(
+                                        'Pick an image',
+                                        const Color(0xFFFF0000)
+                                      ),
+                                      toastDuration: const Duration(seconds: 1),
+                                      fadeDuration: 200
+                                    );
+                                  }
+                                  else if (sub) {
+                                    if (_formKey.currentState!.validate()) {
+                                      final Orders orders = Orders(
+                                        widget.name,
+                                        '',
+                                        ctrlDesc.text,
+                                        '',
+                                        ctrlContact.text
+                                      );
+                                      const Pendings pendings = Pendings(
+                                        'Sent, Waiting for approval',
+                                        ''
+                                      );
+                                      await OrdersAuth.addRequest(orders, pendings, imgFile!).then((value) {
+                                        if (value == true) {
+                                          setState(() => load = false);
+                                          ft.showToast(
+                                            child: Activity.showToast(
+                                              'Sent',
+                                              Colors.blue
+                                            ),
+                                            toastDuration: const Duration(seconds: 1),
+                                            fadeDuration: 200
+                                          );
+                                          clearForm();
+                                        }
+                                      });
+                                    }
+                                    Navigator.pushNamedAndRemoveUntil(context, '/main', (Route<dynamic> route) => false);
+                                  }
+                                  else {
+                                    setState(() => load = false);
+                                    ft.showToast(
+                                      child: Activity.showToast(
+                                        'No internet connection',
+                                        const Color(0xFFFF0000)
+                                      ),
+                                      toastDuration: const Duration(seconds: 1),
+                                      fadeDuration: 200
+                                    );
+                                  }
+                                }
+                                : null,
+                                style: ButtonStyle(
+                                  overlayColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? Colors.blue
+                                    : null;
+                                  }),
+                                  foregroundColor: MaterialStateProperty.resolveWith((states) {
+                                    return states.contains(MaterialState.pressed)
+                                    ? const Color(0xFF00FF00)
+                                    : null;
+                                  }),
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))
+                                  )
+                                ),
+                                icon: const Icon(Icons.shopping_bag),
+                                label: Row(
+                                  children: const [
+                                    Spacer(),
+                                    Text(
+                                      'Order Template',
+                                      style: TextStyle(fontSize: 20)
+                                    ),
+                                    Spacer()
+                                  ]
+                                )
+                              )
+                            )
+                          )
+                        ]
+                      )
+                    )
+                  ]
+                )
+              ]
+            )
+          )
+        ),
+        load == true
+        ? Activity.sent()
+        : Container()
+      ]
+    );
   }
 }
