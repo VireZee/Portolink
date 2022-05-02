0698e6421274dbb75ae5b08cf85e0797d6037625
unknown <jemmanuel@student.ciputra.ac.id>
2022-05-02 20:35:41 +0700

    Update Order

---
+++ lib/views/updateorder.dart
@@ -0,0 +1,14 @@
+part of 'views.dart';
+
+class UpdateOrder extends StatefulWidget {
+  const UpdateOrder({Key? key, required this.orders}) : super(key: key);
+  final Orders orders;
+  @override
+  State<UpdateOrder> createState() => _UpdateOrderState();
+}
+class _UpdateOrderState extends State<UpdateOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
